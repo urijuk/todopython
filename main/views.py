3e877f7702c020c6c997251adfb5fea79cc95585
urijuk <gelte@mail.ru>
2021-01-08 23:52:24 +0600

    added second page and url

--- main/views.py
+++ main/views.py
@@ -5,3 +5,5 @@ def homepage(request):
     return HttpResponse("hello Urmat")
 def test(request):
     return render(request, "test.html")
+def second(request):
+    return HttpResponse("test 2 page")
