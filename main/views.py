3004efa2ada2cbf7aa8569c765c7229902829f04
urijuk <gelte@mail.ru>
2021-01-09 14:34:29 +0600

    Added page3

--- main/views.py
+++ main/views.py
@@ -7,3 +7,5 @@ def test(request):
     return render(request, "test.html")
 def second(request):
     return HttpResponse("test 2 page")
+def third(request):
+    return HttpResponse("This is page test3")
