092b54706930b27a478e4a82a2dd5c242d4ec040
urijuk <gelte@mail.ru>
2021-01-23 20:13:59 +0600

    green

--- todo/urls.py
+++ todo/urls.py
@@ -31,9 +31,11 @@ urlpatterns = [
     path("test6/", delete),
     path("books/", books),
     path("add-todo/", add_todo, name="add-todo"),
-    path("add-book/", add_book, name="add-book"),
     path("delete-todo/<id>", delete_todo, name="delete-todo"),
-    path("mark-todo/<id>", mark_todo, name="mark-todo")
+    path("mark-todo/<id>", mark_todo, name="mark-todo"),
+    path("add-book/", add_book, name="add-book"),
+    path("delete-book/<id>", delete_book, name="delete-book"),
+    path("mark-book/<id>", mark_book, name="mark-book")
 ]   + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
     + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)    
 
